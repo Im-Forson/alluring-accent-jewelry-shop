8499a08bdd91eedbd1bf51d4d32f372449bfb6f6
Samuel-Le-o <105436871+Samuel-Le-o@users.noreply.github.com>
2026-05-19 18:17:40 +0000

    Footer

---
+++ src/components/Footer.jsx
@@ -0,0 +1,67 @@
+import '../App.css'
+import { FaInstagram, FaFacebookF, FaWhatsapp, FaMapMarkerAlt, FaPhoneAlt, FaEnvelope, FaPinterestP, FaTiktok } 
+from "react-icons/fa";
+import logo from '../assets/logo.png'
+
+export default function Footer() {
+    return (
+         <footer className="footer">
+        
+          <div className="footer-container">
+        
+            {/* BRAND SECTION */}
+            <div className="footer-brand">
+                <img src={logo} alt="Alluring Accent Logo" className="w-[150px]" />
+        
+              <p className="copyright">
+                © 2025 ALLURING ACCENT SHOP. All Rights Reserved.
+              </p>
+            </div>
+        
+            {/* QUICK LINKS */}
+            <div className="footer-links">
+              <h3>QUICK LINKS</h3>
+        
+              <a href="#">Home</a>
+              <a href="#">Shop</a>
+              <a href="#">Contact</a>
+            </div>
+        
+            {/* CUSTOMER CARE */}
+            <div className="footer-links">
+              <h3>CUSTOMER CARE</h3>
+        
+              <a href="#">FAQs</a>
+              <a href="#">Shipping & Delivery</a>
+              <a href="#">Track Order</a>
+            </div>
+        
+            {/* CONTACT */}
+            <div className="footer-links">
+              <h3>CONTACT US</h3>
+              <p>+233 20 000 0000</p>
+              <p>alluringaccent@gmail.com</p>
+              <p>Accra, Ghana</p>
+            </div>
+        
+            {/* SOCIALS */}
+            <div className="footer-links">
+              <h3>FOLLOW US</h3>
+              <div className="social-icons">
+                < FaInstagram />
+                <FaFacebookF />
+                <FaTiktok />
+                <FaPinterestP />
+              </div>
+        
+        
+        
+            </div>
+        
+          </div>
+        </footer>
+                
+                
+            
+    )
+}
--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -2,101 +2,110 @@ import '../App.css'
 import NavBar from '../components/NavBar'
 import { Settings } from 'lucide-react'
 import {
-  FaInstagram,
-  FaFacebookF,
-  FaWhatsapp,
-  FaMapMarkerAlt,
-  FaPhoneAlt,
-  FaEnvelope,
+    FaInstagram,
+    FaFacebookF,
+    FaWhatsapp,
+    FaMapMarkerAlt,
+    FaPhoneAlt,
+    FaEnvelope,
+    FaPinterestP,
+    FaTiktok,
 } from "react-icons/fa";
+import Footer from '../components/Footer'
+
+
 
 export default function Contact() {
     return (
+
+
         <div>
-            <NavBar activePage={'contact'}/>
+            <NavBar activePage={'contact'} />
+
             <div className="contact-page bg-[linear-gradient(90deg,#f7e9ea_0%,#e9d4d2_40%,#d8b1ad_75%,#c7938f_100%)]">
+                {/* LEFT SIDE */}
+                <div className="contact-info">
 
-      {/* LEFT SIDE */}
-      <div className="contact-info">
+                    <h1>Let’s Talk</h1>
 
-        <h1>Let’s Talk</h1>
+                    <p>
+                        Have questions about our jewelry collections, custom orders,
+                        or deliveries? We would love to hear from you.
+                    </p>
 
-        <p>
-          Have questions about our jewelry collections, custom orders,
-          or deliveries? We would love to hear from you.
-        </p>
+                    <div className="info-box">
+                        <FaMapMarkerAlt className="info-icon" />
+                        <span>Accra, Ghana</span>
+                    </div>
 
-        <div className="info-box">
-          <FaMapMarkerAlt className="info-icon" />
-          <span>Accra, Ghana</span>
+                    <div className="info-box">
+                        <FaPhoneAlt className="info-icon" />
+                        <span>+233 24 000 0000</span>
+                    </div>
+
+                    <div className="info-box">
+                        <FaEnvelope className="info-icon" />
+                        <span>alluringaccent@gmail.com</span>
+                    </div>
+
+                    {/* SOCIALS */}
+                    <div className="socials">
+                        <a href="#">
+                            <FaInstagram />
+                        </a>
+
+                        <a href="#">
+                            <FaFacebookF />
+                        </a>
+
+                        <a href="#">
+                            <FaWhatsapp />
+                        </a>
+                    </div>
+
+                </div>
+
+                {/* RIGHT SIDE */}
+                <div className="contact-form-container w-[300px] md:w-[700px]">
+
+                    <form className="contact-form">
+
+                        <h2>Send a Message</h2>
+
+                        <div className="input-group">
+                            <label>Full Name</label>
+                            <input type="text" placeholder="Enter your name" />
+                        </div>
+
+                        <div className="input-group">
+                            <label>Email Address</label>
+                            <input type="email" placeholder="Enter your email" />
+                        </div>
+
+                        <div className="input-group">
+                            <label>Subject</label>
+                            <input type="text" placeholder="Enter subject" />
+                        </div>
+
+                        <div className="input-group">
+                            <label>Message</label>
+                            <textarea
+                                rows="6"
+                                placeholder="Write your message..."
+                            ></textarea>
+                        </div>
+
+                        <button type="submit">Send Message</button>
+
+                    </form>
+
+                </div>
+
+            </div>
+            <Footer />
+
         </div>
 
-        <div className="info-box">
-          <FaPhoneAlt className="info-icon" />
-          <span>+233 24 000 0000</span>
-        </div>
 
-        <div className="info-box">
-          <FaEnvelope className="info-icon" />
-          <span>alluringaccent@gmail.com</span>
-        </div>
-
-        {/* SOCIALS */}
-        <div className="socials">
-          <a href="#">
-            <FaInstagram />
-          </a>
-
-          <a href="#">
-            <FaFacebookF />
-          </a>
-
-          <a href="#">
-            <FaWhatsapp />
-          </a>
-        </div>
-
-      </div>
-
-      {/* RIGHT SIDE */}
-      <div className="contact-form-container w-[300px] md:w-[700px]">
-
-        <form className="contact-form">
-
-          <h2>Send a Message</h2>
-
-          <div className="input-group">
-            <label>Full Name</label>
-            <input type="text" placeholder="Enter your name" />
-          </div>
-
-          <div className="input-group">
-            <label>Email Address</label>
-            <input type="email" placeholder="Enter your email" />
-          </div>
-
-          <div className="input-group">
-            <label>Subject</label>
-            <input type="text" placeholder="Enter subject" />
-          </div>
-
-          <div className="input-group">
-            <label>Message</label>
-            <textarea
-              rows="6"
-              placeholder="Write your message..."
-            ></textarea>
-          </div>
-
-          <button type="submit">Send Message</button>
-
-        </form>
-
-      </div>
-
-    </div>
-
-           
-        </div>
     )
 }
