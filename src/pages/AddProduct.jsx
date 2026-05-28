a96ffa750a84ec5a85f1bfc18a2bc6f3f5ac0160
Im-Forson <github.com/Im-Forson>
2026-05-28 19:54:40 +0000

    add products

--- src/pages/AddProduct.jsx
+++ src/pages/AddProduct.jsx
@@ -40,8 +40,6 @@ function AddProduct() {
   const [facingMode, setFacingMode] = useState("environment");
   const [isCameraOpen, setIsCameraOpen] = useState(false);
 
-  const [photos, setPhotos] = useState([]);
-
   const filteredTags = availableTags.filter((tag) =>
     tag.toLowerCase().includes(tagInput.toLowerCase())
   );
@@ -128,7 +126,7 @@ function AddProduct() {
         return;
       }
 
-      if (photos.length === 0) {
+      if (mediaFiles.length === 0) {
         toast.dismiss(loadId);
         toast.error("Image upload is required", {duration: 2000});
         setSubmitting(false);
@@ -170,8 +168,8 @@ function AddProduct() {
         formData.append("colors", color);
       });
 
-      photos.forEach((file, index) => {
-        formData.append(`images`, file);
+      mediaFiles.forEach((file) => {
+        formData.append("images", file);
       });
 
       if (tagInput.trim() === "") {
@@ -179,9 +177,8 @@ function AddProduct() {
       }
 
       const response = await axios.post(
-        'http://localhost:1000/api/product/create', 
+        `${import.meta.env.VITE_API_BASE_URL}/product/create`, 
         formData,
-
         {
           headers: {
             authorization: `Bearer ${localStorage.getItem("ACCESS_TOKEN")}`,
@@ -196,7 +193,7 @@ function AddProduct() {
 
         setColors([]);
         setColor('');
-        setPhotos([]);
+        setMediaFiles([]);
         setProductsArray([]);
         setProductsArray(null);
         setMainIndex(0);
@@ -266,12 +263,6 @@ function AddProduct() {
     if (input) input.click();
   };
 
-  
-  const handlePhotoChange = (e) => {
-    const files = Array.from(e.target.files);
-    setPhotos([...photos, ...files]);
-};
-
   return (
     <div className={`admin-layout ${sidebarOpen ? 'sidebar-active' : ''}`}>
       <SideBar isOpen={sidebarOpen} setIsOpen={setSidebarOpen} />
@@ -291,49 +282,7 @@ function AddProduct() {
         </header>
 
         <form className="product-form" onSubmit={handleSubmit}>
-          <div>
-            <h3 className="text-xl font-semibold mb-4">Product Photos</h3>
-            <input
-                type="file"
-                multiple
-                id="images"
-                required
-                accept="image/*"
-                className="border border-[#7F8C8D] shadow-sm p-2 rounded w-full"
-                onChange={handlePhotoChange}
-            />
-
-            {/* Preview */}
-            {photos.length > 0 && (
-                <div className="mt-4 grid grid-cols-2 md:grid-cols-4 gap-4">
-                    {photos.map((file, index) => {
-                        const objectUrl = URL.createObjectURL(file);
-                        return (
-                            <div key={index} className="relative group">
-                                <img
-                                    src={objectUrl}
-                                    alt={`preview-${index}`}
-                                    className="w-full h-32 object-cover rounded border"
-                                />
-                                <button
-                                    type="button"
-                                    onClick={() => {
-                                        const updatedPhotos = [...photos];
-                                        updatedPhotos.splice(index, 1);
-                                        setPhotos(updatedPhotos);
-                                        URL.revokeObjectURL(objectUrl);
-                                    }}
-                                    className="absolute top-1 right-1 bg-slate-500 text-white rounded-full w-6 h-6 text-xs font-bold flex items-center justify-center"
-                                >
-                                    X
-                                </button>
-                            </div>
-                        );
-                    })}
-                </div>
-            )}
-          </div>
-          {/* <div className="media-upload-card">
+          <div className="media-upload-card">
             <div className="media-upload-header">
               <h3>Product Media</h3>
               <span className="media-count">{mediaFiles.length} file(s) uploaded</span>
@@ -388,7 +337,6 @@ function AddProduct() {
 
           <input
             type="file"
-            name="images"
             id="productMedia"
             hidden
             multiple
@@ -444,7 +392,7 @@ function AddProduct() {
                 );
               })}
             </div>
-          )} */}
+          )}
 
           <div className="input-group">
             <input type="text" name="name" placeholder="Product Name" className="form-input" required />
